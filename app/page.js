f424e52a5239ad53fb9202a933665e4d3daa3d19
Johan Carinus <jcarinus@codestashstudio.com>
2024-05-14 05:28:53 +0200

    Reverted to previous font-loading configuration.

--- app/page.js
+++ app/page.js
@@ -1,19 +1,9 @@
 'use client'
 
 import PasswordGenerator from '@/components/password-generator';
-import localFont from 'next/font/local';
-
-const jetBrainsMonoFont = localFont({ src: '../public/fonts/static/JetBrainsMono-Bold.ttf' });
 
 export default function Home() {
     return (
-        <>
-            <style jsx global>{`
-                html {
-                    font-family: ${jetBrainsMonoFont.style.fontFamily};
-                }
-            `}</style>
-            <PasswordGenerator/>
-        </>
+        <PasswordGenerator/>
     );
 }
